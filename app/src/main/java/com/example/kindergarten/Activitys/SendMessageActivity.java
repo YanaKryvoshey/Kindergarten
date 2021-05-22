069946678fc5831ef7143b69af803d4984037506
YanaKryvoshey <YanaKryvoshey@gmail.com>
2021-05-22 16:48:35 +0300

    add pic

--- app/src/main/java/com/example/kindergarten/Activitys/SendMessageActivity.java
+++ app/src/main/java/com/example/kindergarten/Activitys/SendMessageActivity.java
@@ -1,35 +1,22 @@
 package com.example.kindergarten.Activitys;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.example.kindergarten.Objects.Chats;
-import com.example.kindergarten.Objects.Child;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.kindergarten.Objects.Teacher;
 import com.example.kindergarten.R;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 
 public class SendMessageActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
